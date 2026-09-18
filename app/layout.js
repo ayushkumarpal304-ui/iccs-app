5150d5878c0b36e742078755eeeda0af01448c76
ayushkumarpal304-ui <ayushkumarpal304@gmail.com>
2026-09-18 21:29:26 +0530

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,9 @@
 export const metadata = {
   title: 'ICCS // Intelligent Campus Cognitive System',
   description: 'Controlled by Ayush Kumar Pal',
+  icons: {
+    icon: '/icon.jpg', // यह वर्सेल को साफ़ निर्देश देगा कि आपकी अपलोड की हुई इमेज को ही ऐप का लोगो बनाए
+  },
 }
 
 export default function RootLayout({ children }) {
